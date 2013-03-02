3c7f53e4677dcf8a9c52604ce337454f0e105f44
Joel Silva <joelsilva_87@hotmail.com>
2013-03-02 15:49:32 -0300

    changed url to save image in google drive

--- problemasCiudad.rb
+++ problemasCiudad.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'fusion_tables'
 require "google_drive"
+require "thin"
 require 'httparty'
 
 get '/' do
@@ -14,11 +15,13 @@ post '/form' do
   latitud = response.parsed_response['resultado']['x']
   longitud = response.parsed_response['resultado']['y']
   coordenadas = longitud + ", "+ latitud  
-  
+  puts response
+  puts coordenadas
   session = GoogleDrive.login("contacto@lanestorvive.org", "soloyolase")
+  puts session
   tmpfile = params[:file][:tempfile]
   foto = session.upload_from_file(tmpfile, params[:file][:filename].to_s, :convert => false)
-  url_foto = "https://googledrive.com/host/0B98LKQ7xNbvTNlNnOEFqUHZ3T2s/" + foto.resource_id.gsub("file:", '')
+  url_foto = "https://googledrive.com/host/" + foto.resource_id.gsub("file:", '')
   root = session.root_collection
   collection = root.subcollection_by_title("FotosProblemasCiudad")
 	collection.add(foto)
@@ -28,6 +31,6 @@ post '/form' do
   ft.clientlogin('contacto@lanestorvive.org', 'soloyolase') 
   ft.set_api_key('AIzaSyBbQFCNlmnjpwYtGKMOU6I522e0rP1XqZY') # obtained from the google api console
   ft.execute "INSERT INTO 15nwq3vUOnMwxqnH8LSMVyWu3YsH0aIBck2b2No0 (tipoDeProblema, descripcion, direccion, coordenadas, fecha, urlMedia) VALUES ('#{params[:tipoDeProblema]}', '#{params[:descripcion]}', '#{params[:direccion]}', '" + coordenadas +  "', '#{params[:fecha]}', '"+ url_foto +"');"
-
+   puts ft
    erb :form
 end  
